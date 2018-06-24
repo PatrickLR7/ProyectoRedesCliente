4cc807831ae22dbe84572bb166b6a1c1ccad0506
raquel <mayreneanchia08@gmail.com>
2018-06-24 14:43:09 -0600

    Se implementa el envio de mensajes desde el cliente.

--- src/ClienteUDP.java
+++ src/ClienteUDP.java
@@ -16,7 +16,7 @@ public class ClienteUDP {
             byte[] mensajeByte = mensaje.getBytes(); //Convierte el mensaje en una cadena de bytes.
             InetAddress host = InetAddress.getByName("127.0.0.1");
             int puerto = 9107;
-            DatagramPacket paquete = new DatagramPacket(mensajeByte, mensaje.length(), host, puerto);
+            DatagramPacket paquete = new DatagramPacket(mensajeByte, mensaje.length(), host, puerto);  //Paquete a enviar.
             socket.send(paquete); //El socket envia el paquete.
         }
         catch(SocketException e) { //Manejo de excepcion para el datagram socket.
